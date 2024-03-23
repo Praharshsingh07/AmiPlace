9a0feb71253dd335cb661404552110fd266cf2a0
anush <anushmk85@gmail.com>
2024-03-23 22:54:30 +0530

    new App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,25 @@
-import { BrowserRouter as Router,Routes, Route, Navigate } from 'react-router-dom';
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Navigate,
+} from "react-router-dom";
 import Login from "./Pages/Login";
 import Signup from "./Pages/Signup";
 
 function App() {
+  const router = createBrowserRouter([
+    { path: "/", element: <SignUpPage /> },
+    { path: "/SignUpPage", element: <SignUpPage /> },
+    { path: "/Login", element: <Login /> },
+    { path: "/Login/Forgot_pass", element: <Forgot_pass /> },
+    { path: "/Reset_Success", element: <Reset_Success /> },
+  ]);
+
   return (
-    <Router>
-        <Routes>
-        <Route path="/" element={<Navigate to="/Pages/Login" replace />} />
-        <Route path="/Pages/Login" element={<Login />} />
-        <Route path="/Pages/Signup" element={<Signup />} />
-      </Routes>
-    </Router>
+    <>
+      <RouterProvider router={router} />
+    </>
   );
 }
 
