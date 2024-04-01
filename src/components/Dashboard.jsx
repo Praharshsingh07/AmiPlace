b301c47517101288083601dda313a618e6cbb4b4
devansh verma <devanshverma@devanshs-MacBook-Air-2.local>
2024-04-01 22:56:56 +0530

    devansh update

--- src/components/Dashboard.jsx
+++ src/components/Dashboard.jsx
@@ -28,6 +28,7 @@ const Dashboard = () => {
         <PostListContainer />
         <RecentUpdates />
       </div>
+<<<<<<< HEAD
       <a
         href="#"
         class="mt-7 block w-full rounded-md bg-indigo-600 px-3 py-2 text-center text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
@@ -35,6 +36,8 @@ const Dashboard = () => {
       >
         Logout
       </a>
+=======
+>>>>>>> d77a119 (devansh update)
     </>
   );
 };
