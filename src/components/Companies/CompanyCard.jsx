57d3e0a17abb77cbd2ec9437d497be9f6c0522d2
Rohit21415 <140307199+Rohit21415@users.noreply.github.com>
2024-04-21 14:21:59 +0530

    Committed!!

--- src/components/Companies/CompanyCard.jsx
+++ src/components/Companies/CompanyCard.jsx
@@ -1,14 +1,15 @@
 import React from "react";
 import CompanyDetails from "./CompanyDetails";
+import { Link } from "react-router-dom";
 
 export default function CompanyCard(props) {
   return (
-    <div className="bg-white border-[1px] border-black w-[823px] mb-5 mr-5 p-5 rounded hover:shadow-xl hover:shadow-black hover:duration-500">
+    <div className="bg-black text-white border-[1px] border-white w-[823px] mb-5 mr-5 p-5 rounded hover:shadow-md hover:shadow-gray-300 hover:duration-500">
       <div className="flex">
         <img
           src={props.image}
           alt=""
-          className="w-12 border-[1px] border-black rounded"
+          className="w-12 border-[1px] border-white rounded bg-white"
         />
         <h1 className="font-black ml-3 text-xl">{props.name}</h1>
       </div>
@@ -25,13 +26,8 @@ export default function CompanyCard(props) {
 
       <h1 className="font-bold mt-2">Description</h1>
       <h3>{props.desc}</h3>
-      <button
-        className="bg-[#285375] text-white p-1 rounded-lg ml-[670px] mt-5 px-2 hover:bg-[#80AA84] hover:text-black hover:duration-500"
-        onClick={() => {
-          <CompanyDetails></CompanyDetails>;
-        }}
-      >
-        View Details
+      <button className="bg-[#285375] text-white p-1 rounded-lg ml-[670px] mt-5 px-2 hover:bg-[#80AA84] hover:text-black hover:duration-500">
+        <Link to="/details">View Details</Link>
       </button>
     </div>
   );
